93991be7a876f7c334f406bd8d97512edc02918f
urbanmaid <70835212+urbanmaid@users.noreply.github.com>
2025-03-19 16:49:46 +0900

    Added separation between private and other variables

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -15,25 +15,26 @@ public class PlayerController : MonoBehaviour
 
     [Header("Control")]
     [SerializeField] float moveSpeed = 8f;
+    [SerializeField] float moveSpeedAfterLand = 8f;
     [SerializeField] float moveSpeedStunned = 3.5f;
-    [SerializeField] float moveSpeedCur;
+    private float _moveSpeedCur;
     [SerializeField] float jumpPower = 10f;
     [SerializeField] float crouchPower = 6f;
     [SerializeField] int jumpAmount = 1;
-    private int jumpAmountCur;
-    private bool isRiskyToLand = false;
-    private bool isUsingRigidbody;
-    private InputActions inputActions;
-    [SerializeField] Vector3 movement;
-    [SerializeField] Vector2 moveInput;
+    private int _jumpAmountCur;
+    private bool _isRiskyToLand = false;
+    private bool _isUsingRigidbody;
+    private InputActions _inputActions;
+    private Vector3 _movement;
+    private Vector2 _moveInput;
 
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         rb = GetComponent<Rigidbody>();
-        jumpAmountCur = jumpAmount;
-        moveSpeedCur = moveSpeed;
+        _jumpAmountCur = jumpAmount;
+        _moveSpeedCur = moveSpeed;
 
         //Control setting
         AssignControl();
@@ -41,16 +42,16 @@ public class PlayerController : MonoBehaviour
 
     void OnEnable()
     {
-        inputActions.Player.Enable(); // Player Action Map 활성화
+        _inputActions.Player.Enable(); // Player Action Map 활성화
     }
 
     void OnDisable()
     {
-        inputActions.Player.Disable(); // 비활성화 시 입력 끄기
+        _inputActions.Player.Disable(); // 비활성화 시 입력 끄기
     }
 
     // Update is called once per frame
-    internal void Update()
+    internal void DoUpdate()
     {
         Move();
         CheckFallenSpeed();
@@ -58,52 +59,52 @@ public class PlayerController : MonoBehaviour
 
     void AssignControl()
     {
-        inputActions = new InputActions();
-        if(inputActions != null)
+        _inputActions = new InputActions();
+        if(_inputActions != null)
         {
             Debug.Log("inputActions has been loaded");
         }
-        inputActions.Player.Move.performed += ctx => moveInput = ctx.ReadValue<Vector2>();
-        inputActions.Player.Move.canceled += ctx => moveInput = Vector2.zero;
-        inputActions.Player.Jump.performed += ctx => Jump();
-        inputActions.Player.Crouch.performed += ctx => StartCoroutine(Crouch());
+        _inputActions.Player.Move.performed += ctx => _moveInput = ctx.ReadValue<Vector2>();
+        _inputActions.Player.Move.canceled += ctx => _moveInput = Vector2.zero;
+        _inputActions.Player.Jump.performed += ctx => Jump();
+        _inputActions.Player.Crouch.performed += ctx => StartCoroutine(Crouch());
 
-        inputActions.Player.Enable();
+        _inputActions.Player.Enable();
     }
     void CheckFallenSpeed()
     {
         if(rb.linearVelocity.y < -5f)
         {
-            isRiskyToLand = true;
+            _isRiskyToLand = true;
         }
         else
         {
-            isRiskyToLand = false;
+            _isRiskyToLand = false;
         }
     }
 
     void Move()
     {
-        movement = new Vector3(moveInput.x, 0f, moveInput.y).normalized * moveSpeedCur;
-        if(!isUsingRigidbody)
+        _movement = new Vector3(_moveInput.x, 0f, _moveInput.y).normalized * _moveSpeedCur;
+        if(!_isUsingRigidbody)
         {
             // If player tend to stay in wall, make it unable
-            if((triggerShoulderL.isTriggered && moveInput.x > 0f)
-             ||(triggerShoulderR.isTriggered && moveInput.x < 0f))
+            if((triggerShoulderL.isTriggered && _moveInput.x > 0f)
+             ||(triggerShoulderR.isTriggered && _moveInput.x < 0f))
             {
-                movement.x = 0f;
+                _movement.x = 0f;
             }
 
             // Move with linearVelocity
-            rb.linearVelocity = new Vector3(movement.x, rb.linearVelocity.y, movement.z); // Y축은 점프에만 영향
+            rb.linearVelocity = new Vector3(_movement.x, rb.linearVelocity.y, _movement.z); // Y축은 점프에만 영향
         }
     }
 
     void Jump()
     {
-        if(jumpAmountCur > 0)
+        if(_jumpAmountCur > 0)
         {
-            jumpAmountCur--;
+            _jumpAmountCur--;
             if(triggerKnee.isTriggered) // Obstacle Cross
             {
                 CrossObstacle();
@@ -117,7 +118,7 @@ public class PlayerController : MonoBehaviour
                 rb.AddForce(Vector3.up * jumpPower, ForceMode.Impulse);
             }
         }
-        else if(jumpAmountCur == 0)
+        else if(_jumpAmountCur == 0)
         {
             if(triggerShoulderL.isTriggered)
             {
@@ -133,8 +134,8 @@ public class PlayerController : MonoBehaviour
     }
     void CrossObstacle()
     {
-        isUsingRigidbody = true;
-        rb.AddForce(new Vector3(movement.x, 1f * jumpPower, movement.z), ForceMode.Impulse);
+        _isUsingRigidbody = true;
+        rb.AddForce(new Vector3(_movement.x, 1f * jumpPower, _movement.z), ForceMode.Impulse);
         Debug.Log("Crossing Obstacle");
     }
     public void ResetJumpStatus()
@@ -144,39 +145,47 @@ public class PlayerController : MonoBehaviour
 
     IEnumerator LandCo()
     {
-        jumpAmountCur = jumpAmount;
-        isUsingRigidbody = false;
-        if(isRiskyToLand)
+        _jumpAmountCur = jumpAmount;
+        _isUsingRigidbody = false;
+        if(_isRiskyToLand)
         {
-            moveSpeedCur = moveSpeedStunned;
+            _moveSpeedCur = moveSpeedStunned;
             Debug.LogWarning("You are too fast to land off without injury");
 
-            yield return new WaitForSeconds(1f);
-            moveSpeedCur = moveSpeed;
+            yield return new WaitForSeconds(0.75f);
+            _moveSpeedCur = moveSpeed;
+        }
+        else
+        {
+            _moveSpeedCur = moveSpeedAfterLand;
+            Debug.Log("You have landed");
+
+            yield return new WaitForSeconds(0.4f);
+            _moveSpeedCur = moveSpeed;
         }
     }
 
     void WallKickL()
     {
-        isUsingRigidbody = true;
+        _isUsingRigidbody = true;
         rb.AddForce(jumpPower * new Vector3(-1.6f, 1f, 0f), ForceMode.Impulse);
     }
 
     void WallKickR()
     {
-        isUsingRigidbody = true;
+        _isUsingRigidbody = true;
         rb.AddForce(jumpPower * new Vector3(1.6f, 1f, 0f), ForceMode.Impulse);
     }
 
     IEnumerator Crouch()
     {
-        if(jumpAmountCur > 0)
+        if(_jumpAmountCur > 0)
         {
-            isUsingRigidbody = true;
-            rb.AddForce(crouchPower * new Vector3(movement.x, rb.linearVelocity.y, movement.z), ForceMode.Impulse);
+            _isUsingRigidbody = true;
+            rb.AddForce(crouchPower * new Vector3(_movement.x, rb.linearVelocity.y, _movement.z), ForceMode.Impulse);
 
             yield return new WaitForSeconds(0.5f);
-            isUsingRigidbody = false;
+            _isUsingRigidbody = false;
         }
     }
 
